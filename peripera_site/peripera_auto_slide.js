c9e5a21f187fe4f8da3b5725c1abbb1955cf2e33
Jisu Kim <tlawl912@gmail.com>
2022-05-06 16:55:30 +0900

    commit day+21 슬라이드 해결(미쳤음)/ 다음에는 메뉴바 hover

--- peripera_site/peripera_auto_slide.js
+++ peripera_site/peripera_auto_slide.js
@@ -1,14 +1,15 @@
-let slideIndex1 = 0;
-showSlides();
+let slideIndex = 0;
+autoSlides();
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex1++;
-    if (slideIndex1 > slides.length) { slideIndex1 = 1 }
-    slides[slideIndex1 - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+function autoSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+ 
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(autoSlides, 5000); // Change image every 2 seconds
 }
--- peripera_site/peripera_slide.js
+++ peripera_site/peripera_slide.js
@@ -1,25 +1,21 @@
 let slideIndex2 = 1;
-showSlides(slideIndex2);
-
-function plusSlides(n) {
-    showSlides(slideIndex2 += n);
-}
-
+                    
 function currentSlide(n) {
-    showSlides(slideIndex2 = n);
+  showSlides(slideIndex2 = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let smallImg = document.getElementsByClassName("containerImg_small");
-    if (n > slides.length) { slideIndex2 = 1 }
-    if (n < 1) { slideIndex2 = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < smallImg.length; i++) {
-        smallImg[i].className = smallImg[i].className.replace(" active", "");
-    }
-    slides[slideIndex2 - 1].style.display = "block";
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let button = document.getElementsByClassName("button");
+  if (n > slides.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < button.length; i++) {
+    button[i].className = button[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";  
+  button[slideIndex2-1].className += " active";
 }
